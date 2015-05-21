063cf1dbe39b33764dca1aed49db6578fbb114a2
Michael Ceruzzi <ceruzzi@uchicago.edu>
2015-05-21 16:03:45 -0500

    fixed validations
    fixed validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :topic
 
-  validates :topic_id, :uniqueness =>true, :allow_nil=>false,:presence=>true
+  validates :topic_id, :allow_nil=>false,:presence=>true
   validates :user, :allow_nil=>false,:presence=>true
   validates :post_number, :presence=>true
   validates :content, :allow_nil=>false, :presence=>true
