c596679cd96b1c972a6501f2be456ab5edc4e83c
Michael Ceruzzi <ceruzzi@uchicago.edu>
2015-05-21 16:21:47 -0500

    ok
    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,29 +10,34 @@ Topic.delete_all
 Comment.delete_all
 Subforum.delete_all
 
-# [{:username=> "Michael", :avata_url=> "http://www.wangdicholingresort.com/gifs/ura-village-in-bumthang.jpg", :age=> 24}].each do |usr|
-#   usr=User.new
-#   usr.username=user[:username]
-#   usr.avatar_url=user[:avatar_url]
-#   usr.age=user[:age]
-#   usr.save
-#
-# [{:title=> "test", :subforum=> "School", :cotent=> "Are there any questions about thursday's test"}].each do |thd|
-#   thd=Topic.new
-#   thd.title=topic[:title]
-#   thd.subforum=topic[:subforum]
-#   thd.content=topic[:content]
-#   thd.save
-#
-# [{:topic_id=> 1, :user=> "Michael", :post_number=> 1, :content=> "Looks like there are none."}].each do |pst|
-#   pst=Comment.new
-#   pst.topic_id=post[:topic_id]
-#   pst.user=post[:user]
-#   pst.post_number=post[:post_number]
-#   pst.content=post[:content]
-#   pst.save
-#
-# [{:topic=> "school"},{:topic=> "sports"} ].each do |sub|
-#     sub=Subforum.new
-#     sub.topic=sub[:thread]
-#     sub.save
+[{:username=> "Michael", :avata_url=> "http://www.wangdicholingresort.com/gifs/ura-village-in-bumthang.jpg", :age=> 24}].each do |usr|
+  usr=User.new
+  usr.username=user[:username]
+  usr.avatar_url=user[:avatar_url]
+  usr.age=user[:age]
+  usr.save
+end
+
+[{:title=> "test", :subforum_id=> 1, :cotent=> "Are there any questions about thursday's test"}].each do |thd|
+  thd=Topic.new
+  thd.title=topic[:title]
+  thd.subforum_id=topic[:subforum_id]
+  thd.content=topic[:content]
+  thd.save
+end
+[{:topic_id=> 1, :user=> "Michael", :post_number=> 1, :content=> "Looks like there are none."}].each do |pst|
+  pst=Comment.new
+  pst.topic_id=post[:topic_id]
+  pst.user=post[:user]
+  pst.post_number=post[:post_number]
+  pst.content=post[:content]
+  pst.save
+end
+
+[{:topic=> "school"},{:title=> "sports"} ].each do |sub|
+    sub=Subforum.new
+    sub.topic=sub[:thread]
+    sub.save
+
+
+end
