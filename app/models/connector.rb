8bad1c24ec9e4fb14f1ee267f5011407830a0f9d
Michael Ceruzzi <ceruzzi@uchicago.edu>
2015-05-29 16:31:40 -0500

    Added validations
    Added validations

--- app/models/connector.rb
+++ app/models/connector.rb
@@ -2,4 +2,6 @@ class Connector < ActiveRecord::Base
   belongs_to :topic
   belongs_to :tag
 
+  validates :tag_id, :allow_nil=>false, :presence=>true
+  validates :topic_id, :allow_nil=>false, :presence=>true
 end
